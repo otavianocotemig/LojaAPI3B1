067088d21164d9c9f4976471e7733fa3f75d32d5
otavianocotemig <otaviano@cotemig.com.br>
2020-10-28 11:30:39 -0300

    Get/Post

---
+++ LojaAPI3B1/Controllers/ClienteController.cs
@@ -0,0 +1,37 @@
+﻿using LojaAPI3B1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LojaAPI3B1.Controllers
+{
+    // Definição do Endpoint
+    [RoutePrefix("api/cliente")]
+    public class ClienteController : ApiController
+    {
+        // Lista Provisória para Armazenar os clientes
+        private static List<ClienteModel> listaClientes = new List<ClienteModel>();
+
+        // Metodo responsavel pelo Post no Cliente
+        [AcceptVerbs("POST")]
+        [Route("CadastrarCliente")]
+        public string CadastrarCliente(ClienteModel cliente)
+        {
+            // comando para insert no banco
+            listaClientes.Add(cliente);
+            return "Cliente Cadastrado com Sucesso";
+
+        }
+
+        // Metodo para Listar todos os cliente
+        [AcceptVerbs("GET")]
+        [Route("ConsultarCliente")]
+        public List<ClienteModel> ConsultarClientes()
+        {
+            return listaClientes;
+        }
+    }
+}
---
+++ LojaAPI3B1/Models/ClienteModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LojaAPI3B1.Models
+{
+    public class ClienteModel
+    {
+
+        private int id_cliente;
+        private string nom_cliente;
+        private string sobrenome_cliente;
+        private string email_cliente;
+
+        public ClienteModel()
+        {
+
+        }
+
+        public ClienteModel(int id_cliente, string nom_cliente, string sobrenome_cliente, string email_cliente)
+        {
+            this.id_cliente = id_cliente;
+            this.nom_cliente = nom_cliente;
+            this.sobrenome_cliente = sobrenome_cliente;
+            this.email_cliente = email_cliente;
+        }
+
+        public int Id_cliente { get => id_cliente; set => id_cliente = value; }
+        public string Nom_cliente { get => nom_cliente; set => nom_cliente = value; }
+        public string Sobrenome_cliente { get => sobrenome_cliente; set => sobrenome_cliente = value; }
+        public string Email_cliente { get => email_cliente; set => email_cliente = value; }
+    }
+}
